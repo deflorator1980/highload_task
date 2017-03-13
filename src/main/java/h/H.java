1f7d11894ccd98792ffd8da66dd7a9e2fc22545b
Исаков Андрей <a.isakov@neolant.ru>
2017-03-13 13:02:38 +0300

    заебис!

--- src/main/java/h/H.java
+++ src/main/java/h/H.java
@@ -1,5 +1,8 @@
 package h;
 
+import com.arturmkrtchyan.sizeof4j.SizeOf;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -18,15 +21,31 @@ public class H {
 //
 //        int i = 5 ^ 2;
 //        System.out.println(i);
-        int[] a1 = new int[]{21, 23, 24, 40, 75, 76, 78, 77, 900, 2100, 2200, 2300, 2400, 2500};
-        int[] a2 = new int[]{10, 11, 41, 50, 65, 86, 98, 101, 190, 1100, 1200, 3000, 5000};
-        List<Integer> l1 = Arrays.stream(a1).boxed().collect(Collectors.toList());
-        List<Integer> l2 = Arrays.stream(a2).boxed().collect(Collectors.toList());
-        Collections.shuffle(l1);
-        Collections.shuffle(l2);
-        System.out.println(l1);
-        System.out.println(l2);
-        Collections.sort(l1);
-        System.out.println(l1);
+//        int[] a1 = new int[]{21, 23, 24, 40, 75, 76, 78, 77, 900, 2100, 2200, 2300, 2400, 2500};
+//        int[] a2 = new int[]{10, 11, 41, 50, 65, 86, 98, 101, 190, 1100, 1200, 3000, 5000};
+//        List<Integer> l1 = Arrays.stream(a1).boxed().collect(Collectors.toList());
+//        List<Integer> l2 = Arrays.stream(a2).boxed().collect(Collectors.toList());
+//        Collections.shuffle(l1);
+//        Collections.shuffle(l2);
+//        System.out.println(l1);
+//        System.out.println(l2);
+//        Collections.sort(l1);
+//        System.out.println(l1);
+//        System.out.println("a1: " + SizeOf.shallowSize(a1));
+//        System.out.print("a1: [");
+//        Arrays.stream(a1).forEach(a -> System.out.print(a + " "));
+//        System.out.println("]" + SizeOf.shallowSize(a1));
+//        System.out.println("l1: " + l1 + " " + SizeOf.shallowSize(l1));
+//
+//        System.out.println(l1.get(1));
+//        System.out.println(l1);
+//        System.out.println(a1.length);
+//        System.out.println(l1.size());
+
+        ArrayList<Integer> list = new ArrayList<>(10_000_000);
+        Integer[] arr = list.toArray(new Integer[10_000_000]);
+        list.addAll(Arrays.stream(arr).collect(Collectors.toList()));
+        System.out.println(SizeOf.shallowSize(arr));
+        System.out.println(SizeOf.shallowSize(list));
     }
 }
--- src/main/java/mergesort/MergingFileFromFile.java
+++ src/main/java/mergesort/MergingFileFromFile.java
@@ -3,6 +3,7 @@ package mergesort;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,61 +12,68 @@ public class MergingFileFromFile {
         int[] a1 = new int[]{21, 23, 24, 40, 75, 76, 78, 77, 900, 2100, 2200, 2300, 2400, 2500};
 //        int[] a2 = new int[]{10, 11, 41, 50, 65, 86, 98, 101, 190, 1100, 1200, 3000, 5000};
         int[] a2 = new int[]{30, 31, 41, 50, 65, 86, 98, 101, 190, 1100, 1200, 3000, 5000};
-        int size = a1.length + a2.length;
 
-        RandomAccessFile raf1 = new RandomAccessFile("a1", "rw");
+        List<Integer> l1 = Arrays.stream(a1).boxed().collect(Collectors.toList());
+        List<Integer> l2 = Arrays.stream(a2).boxed().collect(Collectors.toList());
+        Collections.shuffle(l1);
+        Collections.shuffle(l2);
+        Collections.sort(l1);
+        Collections.sort(l2);
+
+        int size = l1.size() + l2.size();
+
+        RandomAccessFile raf1 = new RandomAccessFile("l1", "rw");
         raf1.seek(0);
-        for (int i = 0; i < a1.length; i++) {
-            raf1.writeInt(a1[i]);
+        for (int i = 0; i < l1.size(); i++) {
+            raf1.writeInt(l1.get(i));
         }
 
-        RandomAccessFile raf2 = new RandomAccessFile("a2", "rw");
+        RandomAccessFile raf2 = new RandomAccessFile("l2", "rw");
         raf2.seek(0L);
-        for (int i = 0; i < a2.length; i++) {
-            raf2.writeInt(a2[i]);
+        for (int i = 0; i < l2.size(); i++) {
+            raf2.writeInt(l2.get(i));
         }
 
-        System.out.print("a1: ");
+        System.out.print("l1: ");
         raf1.seek(0);
-        for (int i = 0; i < a1.length; i++) {
+        for (int i = 0; i < l1.size(); i++) {
             System.out.print(raf1.readInt() + " ");
         }
 
-        System.out.print("\na2: ");
+        System.out.print("\nl2: ");
         raf2.seek(0);
-        for (int i = 0; i < a2.length; i++) {
+        for (int i = 0; i < l2.size(); i++) {
             System.out.print(raf2.readInt() + " ");
         }
         System.out.println();
 
-        RandomAccessFile rafRez = new RandomAccessFile("storageRez", "rw");
+        RandomAccessFile rafRez = new RandomAccessFile("lRes", "rw");
 
         int i = 0, j = 0;
         for (int k = 0; k < size; k++) {
-            if (i > ((a1.length - 1) * 4)) {
+            if (i > ((l1.size() - 1) * 4)) {
                 raf2.seek(j);
                 int a = raf2.readInt();
                 rafRez.writeInt(a);
                 j += 4;
+            } else if (j > ((l2.size() - 1) * 4)) {
+                raf1.seek(i);
+                int a = raf1.readInt();
+                rafRez.writeInt(a);
+                i += 4;
             } else {
-                if (j > ((a2.length - 1) * 4)) {
-                    raf1.seek(i);
-                    int a = raf1.readInt();
+                raf1.seek(i);
+                raf2.seek(j);
+                int a = raf1.readInt();
+                int b = raf2.readInt();
+                if (a < b) {
                     rafRez.writeInt(a);
                     i += 4;
                 } else {
-                    raf1.seek(i);
-                    raf2.seek(j);
-                    int a = raf1.readInt();
-                    int b = raf2.readInt();
-                    if (a < b) {
-                        rafRez.writeInt(a);
-                        i += 4;
-                    } else {
-                        rafRez.writeInt(b);
-                        j += 4;
-                    }
+                    rafRez.writeInt(b);
+                    j += 4;
                 }
+
             }
         }
 
